9a83cf664a8269000475ae4cfb80db80d053e7e7
Alexis POirier <alexispoirier@live.com>
2019-06-21 16:14:51 +0200

    Second commit

--- london_tube_graph.rb
+++ london_tube_graph.rb
@@ -23,21 +23,26 @@ london_tube = LondonTube.new(tube_lines)
 
 g = GraphViz.new( :G, :type => :digraph)
 
+existing_nodes = []
+existing_edges = []
+
 london_tube.route_links.each do |rl|
   from = london_tube.coordinates.fetch(rl[:from])
   to = london_tube.coordinates.fetch(rl[:to])
-  g.add_nodes(from)
-  g.add_nodes(to)
-  g.add_edges(from, to)
+
+  unless existing_nodes.include?(from)
+    g.add_nodes(from)
+    existing_nodes << from
+  end
+  unless existing_nodes.include?(to)
+    g.add_nodes(to)
+    existing_nodes << to
+  end
+
+  unless existing_edges.any? { |e| e[:from] == from && e[:to] == to }
+    g.add_edges(from, to)
+    existing_edges << { from: from, to: to }
+  end
 end
 
 g.output(:png => "london_tube.png")
-
-#stations.each_with_index do |s, index|
-#  if stations[index + 1]
-#    g.add_edges(s.name, stations[index + 1].name)
-#    g.add_edges(stations[index + 1].name, s.name)
-#  end
-#end
-
-
