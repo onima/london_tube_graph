9c099844cb158b44ae11552ce8a437efcd3da963
Alexis POirier <alexispoirier@live.com>
2019-06-21 19:24:40 +0200

    Last commit

--- models/graph_generator.rb
+++ models/graph_generator.rb
@@ -1,33 +1,45 @@
 class GraphGenerator
 
-  def initialize(tube:, type:)
+  def initialize(tube:, type:, file_name:)
     @tube = tube
     @g = GraphViz.new( :G, :type => type)
+    @file_name = file_name
     @existing_nodes = []
     @existing_edges = []
   end
 
   def generate!
-    @tube.routes_links.each do |rl|
-      from = @tube.stations_with_stop_point_reference.fetch(rl[:from])
-      to = @tube.stations_with_stop_point_reference.fetch(rl[:to])
+    @tube.lines_routes_links.each do |rl|
+      from = @tube.lines_stations_with_stop_point_reference.fetch(rl[:from])
+      to = @tube.lines_stations_with_stop_point_reference.fetch(rl[:to])
 
-      unless @existing_nodes.include?(from)
+      unless node_exists?(from)
         @g.add_nodes(from)
         @existing_nodes << from
       end
-      unless @existing_nodes.include?(to)
+
+      unless node_exists?(to)
         @g.add_nodes(to)
         @existing_nodes << to
       end
 
-      unless @existing_edges.any? { |e| e[:from] == from && e[:to] == to }
+      unless edge_exists?(from, to)
         @g.add_edges(from, to)
         @existing_edges << { from: from, to: to }
       end
     end
 
-    g.output(:png => "london_tube_graph.png")
+    @g.output(:png => @file_name)
+  end
+
+  private
+
+  def node_exists?(node)
+    @existing_nodes.include?(node)
+  end
+
+  def edge_exists?(from, to)
+    @existing_edges.any? { |e| e[:from] == from && e[:to] == to }
   end
 end
 
